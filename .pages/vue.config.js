2a04f49fd344faa4e30f96b146f1ce56eaba44fd
Taiizor <41683699+Taiizor@users.noreply.github.com>
2024-12-21 17:50:19 +0300

    Editing GitHub Page

--- .pages/vue.config.js
+++ .pages/vue.config.js
@@ -3,5 +3,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false, // ESLint hataları için build'i engellemeyecek
-  publicPath: process.env.NODE_ENV === 'production' ? '/UUID/' : '/'
+  //publicPath: process.env.NODE_ENV === 'production' ? '/UUID/' : '/'
 })
